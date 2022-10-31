1f8f43f382cfbe795a74e78a762294f89f344f48
lai <laila.magallan@hotmail.com>
2022-10-31 20:32:48 -0300

    modificaciones para la 1era entrega

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 let bienvenido = alert ("Bienvenido/a a Arakur Ushuaia Resort & Spa");
+let edadUser = parseInt( prompt ("Ingrese su edad."));
+const edad = 18;
+let esMenor = true;
 
-let edadUser= parseInt(prompt('Ingrese su edad'));
-const edad=18;
-let esMenor =true
+function coincide (edadUser) {
+    return (18 <= edadUser)
+};
 
-while (esMenor){
-if(edadUser>=18){alert("Puedes alojarte con nosotros");
-    console.log('Podes alojarte en nuestro hotel')
-    esMenor=false;}
-    else {
-        edadUser = parseInt( prompt ("Debe ser mayor de edad para poder alojarse con nosotros"))
-        esMenor=true;
-        console.log('No puedes reservar siendo menor, por favor contacta a un mayor')
-    }
+if (coincide(edadUser)) {
+    alert ("Puedes realizar tu reserva en nuestra página");
+    esMenor = false;
 }
+while (esMenor){
+    if(edadUser>=18){alert("Puedes alojarte con nosotros");
+        console.log('Podes alojarte en nuestro hotel')
+        esMenor=false;}
+        else {
+            edadUser = parseInt( prompt ("Debe ser mayor de edad para poder alojarse con nosotros"))
+            esMenor=true;
+            console.log('No puedes reservar siendo menor, por favor contacta a un mayor')
+        }
+    }
